87b04e3dbdc4ebf8e0bac5967f29152f1350556e
islam <iabdelkarim@uplandsoftware.com>
2021-07-30 20:31:09 +0200

    navigate the menu using @Output event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
+
+  currentItem = 'recipes'
+
+  onItemSelected(selectedItem){
+    this.currentItem = selectedItem;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() itemSelected = new EventEmitter();
   collapsed = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onSelect(feature: string){
+    this.itemSelected.emit(feature);
+  }
 }
